3e0a73aa3ffddbb31f653aa879aae706edbf057e
Emilio Davis <emilio.davis@gmail.com>
2015-01-27 18:39:21 -0300

    version 2.0.2

--- khipupayment.php
+++ khipupayment.php
@@ -19,7 +19,7 @@ class KhipuPayment extends PaymentModule {
         $this->description = $this->l('Transferencia bancaria usando khipu');
 
         $this->author = 'khipu';
-        $this->version = '2.0.1';
+        $this->version = '2.0.2';
         $this->tab = 'payments_gateways';
 
 
