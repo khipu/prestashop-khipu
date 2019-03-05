fde564b79e89ee9981607544973c6e64d58a9e30
chaotive <leonardomontenegro123@gmail.com>
2019-03-05 16:56:54 -0300

    Updating release packager

--- package.sh
+++ package.sh
@@ -1,6 +1,18 @@
+#!/usr/bin/env bash
+
 cd ..
-rm -rf khipupayment khipupayment.zip
-cp -R prestashop-khipu khipupayment
-rm -rf khipupayment/.git khipupayment/.gitignore khipupayment/.gitmodules khipupayment/.idea khipupayment/.DS_Store khipupayment/package.sh 
+rm -rf prestashop-khipu-release
+mkdir prestashop-khipu-release
+cp -R prestashop-khipu prestashop-khipu-release
+cd prestashop-khipu-release
+mv prestashop-khipu khipupayment
+rm -rf \
+    khipupayment/.git \
+    khipupayment/.gitignore \
+    khipupayment/.gitmodules \
+    khipupayment/.idea \
+    khipupayment/.DS_Store \
+    khipupayment/package.sh \
+    khipupayment/composer.phar
 zip -r khipupayment.zip khipupayment
-rm -rf khipupayment
+cd ../prestashop-khipu
