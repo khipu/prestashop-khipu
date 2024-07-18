67f4901628b522af9beffa43af5377fe6fe540de
JKacicM <jorge.kacic@khipu.com>
2024-07-18 13:26:51 -0400

    Actualización a API 3.0

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ En este manual se explica como configurar khipu para ser usado como medio de pag
 
 Antes de comenzar, se debe contar con lo siquiente:
 - Una cuenta habilitada para cobrar en khipu.com.
-- Prestashop 1.7
+- Prestashop 1.7 a 8.1
 - El archivo khipupayment.zip, el cual contiene el plugin khipu para Prestashop.
+- requiere tener habilido cURL en el servidor.
 
 [![Tutorial integración khipu en Prestashop](http://img.youtube.com/vi/l2is1lwSd_4/0.jpg)](https://www.youtube.com/watch?v=l2is1lwSd_4 "Tutorial integración khipu en Prestashop")
 
--- validate.php
+++ validate.php
@@ -10,14 +10,13 @@
  * obtain it through the world-wide-web, please send an email
  * to license@prestashop.com so we can send you a copy immediately.
  *
- * @author    khipu <support@khipu.com>
- * @copyright 2007-2020 khipu SpA
- * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @autor       khipu <support@khipu.com>
+ * @copyright   2007-2024 khipu SpA
+ * @license     http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
 
 require_once(dirname(__FILE__) . '/../../config/config.inc.php');
 require_once(dirname(__FILE__) . '/../../init.php');
-require __DIR__ . '/vendor/autoload.php';
 require_once('KhipuPostBack.php');
 
 $notify = new KhipuPostback();
